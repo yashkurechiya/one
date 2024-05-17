e8517b6a3a970210e3e77860ffb0711b4a66a78d
yashkurechiya <2005yashkurechiya@gmail.com>
2024-05-17 17:23:36 +0530

    commit

---
+++ app/src/main/java/com/example/three/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.three
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+       val text =findViewById<TextView>(R.id.text_view)
+        text.setOnClickListener {
+            val intent = Intent(this, yash::class.java)
+            startActivity(intent)
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
